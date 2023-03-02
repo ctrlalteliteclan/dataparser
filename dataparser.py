97cfabdbe170cdfde555306df29f18e49a843f9b
airsquared <36649395+airsquared@users.noreply.github.com>
2023-03-02 14:19:57 -0800

    Initial

---
+++ dataparser.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import sys
+
+# Get command line arguments
+file_name = sys.argv[1]
+output_format = sys.argv[2]
+
+# Read data from file
+data = pd.read_csv(file_name, delimiter='\t')
+
+# Convert to the desired format
+if output_format == '-c':
+    data.to_csv('output.csv', index=False)
+elif output_format == '-j':
+    data.to_json('output.json', orient='records')
+elif output_format == '-x':
+    data.to_xml('output.xml')
+
+print('Output file saved successfully!')
