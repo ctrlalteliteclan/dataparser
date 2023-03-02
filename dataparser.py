13a9846cfb187ca318f737a6dfe54fecde94b2e2
airsquared <36649395+airsquared@users.noreply.github.com>
2023-03-02 15:27:05 -0800

    Final

--- dataparser.py
+++ dataparser.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import sys
+import re
 
-
-print(pd.__version__)
 
 # Get command line arguments
 file_name = sys.argv[1]
@@ -19,6 +18,30 @@ if output_format == '-c':
 elif output_format == '-j':
     data.to_json('output.json', orient='records')
 elif output_format == '-x':
-    data.to_xml('output.xml')
+    data.to_csv('output.csv', index=False)
+    # Read the CSV file and extract the column names and content
+    df = pd.read_csv('output.csv')
+    columns = df.columns.tolist()
+    content = df.values.tolist()
+
+    # Loop through the column names and content and convert them to valid XML tags
+    xml_rows = []
+    for row in content:
+        xml_row = []
+        for i, value in enumerate(row):
+            # Remove any non-alphanumeric characters and replace spaces with underscores
+            xml_column = re.sub('[^0-9a-zA-Z]+', '', columns[i]).replace(' ', '_')
+            # Add opening and closing XML tags to the column name and content
+            xml_column = '<{0}>{1}</{0}>'.format(xml_column, value)
+            xml_row.append(xml_column)
+            xml_rows.append(''.join(xml_row))
+
+    # Write the XML tags and content to a new file
+    with open('output.xml', 'w') as f:
+        f.write('<root>\n')
+        for xml_row in xml_rows:
+            f.write(xml_row + '\n')
+        f.write('</root>')
+    
 
 print('Output file saved successfully!')
