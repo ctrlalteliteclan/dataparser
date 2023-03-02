1780db5f040ce08036f41bbd2fa300422f53b04e
airsquared <36649395+airsquared@users.noreply.github.com>
2023-03-02 14:42:23 -0800

    Add output files

--- dataparser.py
+++ dataparser.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import sys
 
+
+print(pd.__version__)
+
 # Get command line arguments
 file_name = sys.argv[1]
 output_format = sys.argv[2]
 
 # Read data from file
 data = pd.read_csv(file_name, delimiter='\t')
+
+data = data.rename(columns={'Home State': 'Home_State'})
 
 # Convert to the desired format
 if output_format == '-c':
